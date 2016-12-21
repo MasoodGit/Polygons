c94761babbaec1c2eaab047a093ab4a0c7f7b053
Masood_Alam <mohamed.masood.alam@philips.com>
2016-12-22 03:56:18 +0530

    Initial Commit

---
+++ Polygons/Polygons.Library/ConcreteRegularPolygon.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polygons.Library
+{
+    public class ConcreteRegularPolygon
+    {
+        public ConcreteRegularPolygon(int sides,int length)
+        {
+            NumberOfSides = sides;
+            SideLength = length;
+        }
+
+        public int NumberOfSides { get; set; }
+        public int SideLength { get; set; }
+
+        public double GetPerimeter()
+        {
+            return NumberOfSides * SideLength;
+        }
+
+        public virtual double GetArea()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class Square : ConcreteRegularPolygon
+    {
+        public Square(int length) : base(4, length)
+        {
+        }
+
+        public override double GetArea()
+        {
+            return SideLength * SideLength;
+        }
+    }
+}
---
+++ Polygons/Polygons/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Polygons.Library;
+
+namespace Polygons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var square = new Square(5);
+            DisplayPolygon("Square", square);
+
+
+            Console.Read();
+
+        }
+
+        private static void DisplayPolygon(string polygonType, ConcreteRegularPolygon polygon)
+        {
+
+            Console.WriteLine("{0} Number of Sides : {1}",polygonType,polygon.NumberOfSides);
+        }
+    }
+}
