93fe62eddd103ae1bb7d8e021b4f9427dd771cd7
Masood_Alam <mohamed.masood.alam@philips.com>
2016-12-31 11:31:48 +0530

    Update

--- Polygons/Polygons.Library/IRegularPolygon.cs
+++ Polygons/Polygons.Library/IRegularPolygon.cs
@@ -1,40 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Polygons.Library
+﻿namespace Polygons.Library
 {
-   public  interface IRegularPolygon
+    public interface IRegularPolygon
     {
         int NumberOfSides { get; set; }
         int SideLength { get; set; }
 
         double GetArea();
         double GetPerimeter();
-    }
-
-    public class Octagon : IRegularPolygon
-    {
-        public int NumberOfSides { get; set; }
-        public int SideLength { get; set; }
-
-
-        public Octagon(int length)
-        {
-            NumberOfSides = 8;
-            SideLength = length;
-        }
-
-        public double GetArea()
-        {
-            return SideLength * SideLength * (2 + 2 * Math.Sqrt(2));
-        }
-
-        public double GetPerimeter()
-        {
-            return NumberOfSides * SideLength;
-        }
     }
 }
